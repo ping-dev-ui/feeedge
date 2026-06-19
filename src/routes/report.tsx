2be5948205f133f41a786f18755e78ea18f4a359
ping-dev-ui <pingacho@gmail.com>
2026-06-19 12:00:36 +0100

    Fix unused import in report.tsx (build fix)

--- src/routes/report.tsx
+++ src/routes/report.tsx
@@ -1,4 +1,4 @@
-import { createFileRoute, Link } from '@tanstack/react-router'
+import { createFileRoute } from '@tanstack/react-router'
 import { LegalPage } from '~/components/LegalPage'
 import { JsonLd } from '~/components/JsonLd'
 import { ExchangeLogo } from '~/components/ExchangeLogo'
